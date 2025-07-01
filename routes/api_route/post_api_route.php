082dff997294f2fd3f22cd5341611961734b8e83
dewan <shamim.codemoly@gmail.com>
2025-07-01 13:01:42 +0600

    first commit

--- routes/api_route/post_api_route.php
+++ routes/api_route/post_api_route.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\PostController;
 
 Route::get('all-post', [PostController::class, 'all']);
 
+
 Route::middleware(['auth:api'])->group(function () {
     Route::put('posts-status/{id}', [PostController::class, 'status']);
     Route::apiResource('posts', PostController::class);
