7801985af3029e429297cc487e656978788d00fb
dewan <shamim.codemoly@gmail.com>
2025-07-01 15:10:42 +0600

    feat: gtm

---
+++ main_deplyer.sh
@@ -0,0 +1,20 @@
+
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+#(php artisan down)
+    # Update codebase
+    git pull origin dev
+    # Exit maintenance mode
+php artisan up
+# php artisan migrate --force
+# php artisan passport:client --personal --name="ib" --no-interaction
+# php artisan passport:keys --force
+chmod -R 777 storage
+# npm run build
+
+php artisan optimize:clear
+php artisan cache:clear
+echo "Application deployed!"
