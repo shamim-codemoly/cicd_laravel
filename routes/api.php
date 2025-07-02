def6d84b81eb21ffe90e46928bad69c6225f3d01
dewan <shamim.codemoly@gmail.com>
2025-07-02 11:43:55 +0600

    feat: gtm

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-require __DIR__ . '/api_route/category_api_route.php';
-require __DIR__ . '/api_route/post_api_route.php';
+Route::get('/test', function () {
+    return 'aaaaaaaaaaaaaaaaaa';
+});
