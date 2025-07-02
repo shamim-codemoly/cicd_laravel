9ec8a8af4e5a1778338e98a2aa155f2ca1bf8423
dewan <shamim.codemoly@gmail.com>
2025-07-02 12:31:26 +0600

    feat: gtm

--- app/Http/Controllers/GtmConfigController.php
+++ app/Http/Controllers/GtmConfigController.php
@@ -11,9 +11,17 @@ class GtmConfigController extends Controller
     public function gtmServerContainerConfig(Request $request)
     {
         $domain = $request->input('gtm_sub_domain_name');
-        $port = $request->input('port');
+        $port = $this->getNextAvailablePort();
         $email = $request->input('gtm_ssl_email');
         $config = $request->input('gtm_config_id'); // base64 or GTM-ID
+
+        //     GtmDeployment::create([
+        // 'shop_id' => getUserShopId(),
+        //     'domain' => $domain,
+        //     'email' => $email,
+        //     'config' => $config,
+        //     'port' => $port,
+        // ]);
 
         $scriptPath = base_path('deploy.sh');
         $process = new Process([$scriptPath, $domain, $port, $email, $config]);
@@ -29,5 +37,21 @@ class GtmConfigController extends Controller
             'message' => 'Deployed successfully!',
             'output' => $process->getOutput()
         ]);
+    }
+
+    function getNextAvailablePort($start = 5201, $end = 65000)
+    {
+        $usedPorts = collect(explode("\n", shell_exec("ss -tuln | awk '{print $5}' | grep ':' | cut -d':' -f2")))
+            ->filter()
+            ->unique()
+            ->map(fn($port) => intval(trim($port)));
+
+        for ($port = $start; $port <= $end; $port++) {
+            if (!$usedPorts->contains($port)) {
+                return $port;
+            }
+        }
+
+        throw new \Exception("No free ports available");
     }
 }
---
+++ public/koba_samso.php
@@ -0,0 +1,7 @@
+<?php
+echo "Deployed GTM Container<br>";
+echo "Domain: gtm.shamim.com<br>";
+echo "Email: shamim2@gmail.com<br>";
+echo "Port: 5201<br>";
+echo "Config: aWQ9R1RNLUtLWEtaWkhIJmVudj0xJmF1dGg9R2tOcUlEeDJTRWxJRXc4U3lraEkzUQ==<br>";
+?>
