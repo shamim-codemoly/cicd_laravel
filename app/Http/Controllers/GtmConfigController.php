82417fb6bfa5efa0b6d4162a300e2e86cba64073
dewan <shamim.codemoly@gmail.com>
2025-07-02 11:58:07 +0600

    feat: gtm

---
+++ app/Http/Controllers/GtmConfigController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class GtmConfigController extends Controller
+{
+    public function gtmServerContainerConfig(Request $request)
+    {
+        $domain = $request->input('gtm_sub_domain_name');
+        $port = $request->input('port');
+        $email = $request->input('gtm_ssl_email');
+        $config = $request->input('gtm_config_id'); // base64 or GTM-ID
+
+        $scriptPath = '/home/dewan/Music/deploy.sh';
+
+        $process = new Process([$scriptPath, $domain, $port, $email, $config]);
+        $process->setTimeout(300); // 5 minutes
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+
+        return response()->json([
+            'message' => 'Deployed successfully!',
+            'output' => $process->getOutput()
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\GtmConfigController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return 'aaaaaaaaaaaaaaaaaa';
-});
+
+    Route::post('/gtm-server-container-config', [GtmConfigController::class, 'gtmServerContainerConfig']);
